11911124b4d789cda0540a84bad9a3f449d339ce
Alba Martinez <albalanaja94@gmail.com>
2018-02-11 18:35:32 +0100

    practica desarrollo entorno cliente

---
+++ templates/formulario.php
@@ -0,0 +1,25 @@
+<div class="cuerpo">
+<img class="cabecera" src="../images/social-media.jpg" alt="">
+<div class="body">
+<h1>Introduce tus datos</h1>
+<form>
+	<div class="nombre">
+		<label>Nombre:</label>
+		<input id="nombre" type="text" placeholder="Introduce tu nombre">
+	</div>
+	<div class="email">
+		<label>Email:</label>
+		<input id="email" type="email" placeholder="Introduce tu email">
+	</div>
+	<div class="dni">
+		<label>DNI:</label>
+		<input id="dni" type="text" placeholder="Introduce tu dni">
+	</div>
+	<div class="edad">
+		<label>Edad:</label>
+		<input id="edad" type="text" placeholder="Introduce tu edad">
+	</div>
+	<input type="submit" value="Enviar">
+</form>
+</div>
+</div>
---
+++ templates/index.php
@@ -0,0 +1,50 @@
+<?php
+if(isset($_REQUEST['id'])){
+	$id = $_REQUEST['id'];
+}else{
+	$id = 'presentacion';
+}
+?>
+<!-- ?id=url -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+	<header id="header" class="">
+		<div class="all">
+			<div class="logo">RSS</div>
+		<nav class="menu">
+			<ul>
+				<li class="home"><a class="enlace" href='/'>Home</a></li>
+				<li class="inscribete"><a class="enlace" href='?id=formulario'>Inscríbete</a></li>
+				<li class="ver"><a class="enlace" href='?id=lista'>Ver todas las suscripciones</a></li>
+			</ul>
+		</nav>
+	</div>
+	</header><!-- /header -->
+	<?php 
+		include_once($id . '.php');
+	?>
+	<footer>
+		<div class="all">
+		<div class="madein">
+			<p>Alba Martínez Labrador</p>
+		</div>
+		<div class="social-networks">
+			<a href="#" class="fb">Facebook</a>
+			<a href="#" class="tw">Twitter</a>
+			<a href="#" class="ig">Instagram</a>
+			<a href="#" class="lk">LinkedIn</a>
+		</div>
+	</div>
+	</footer>
+</body>
+
+</html>
